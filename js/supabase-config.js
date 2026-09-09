272840504a2a5b29d951d6eab5583656b1323651
Adoretty <301391707+Adoretty@users.noreply.github.com>
2026-09-09 17:40:50 -0400

    Primeras lineas para conexion a la BD

---
+++ js/supabase-config.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = 'https://uoxtbzfmznnvhjdwdlrr.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_Ro3jxG0zgAYjJeaj97h02A_DcSYKCgh';
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
